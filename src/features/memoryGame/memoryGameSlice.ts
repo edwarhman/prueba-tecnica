e52be8529ab778767c571cb4d1394471c95752bc
Emerson Warhman <edwarhman@gmail.com>
2022-07-10 12:58:32 -0400

    fix: slice export feature

--- src/features/memoryGame/memoryGameSlice.ts
+++ src/features/memoryGame/memoryGameSlice.ts
@@ -1,45 +1,50 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { stat } from 'fs';
-import { RootState, AppThunk } from '../../app/store';
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { stat } from "fs";
+import { RootState, AppThunk } from "../../app/store";
 
 const TOTAL_PAIRS = 12;
 
 export interface MemoryGame {
-    failsCount: number,
-    score: number,
-    cardsMatrix: string[][],
-    pairs: number,
-    triesToSelect: number
+  failsCount: number;
+  score: number;
+  cardsMatrix: string[][];
+  pairs: number;
+  triesToSelect: number;
 }
 
 const initialState: MemoryGame = {
-    failsCount: 0,
-    score: 0,
-    cardsMatrix: [[],[]],
-    pairs: TOTAL_PAIRS,
-    triesToSelect: 2
-}
+  failsCount: 0,
+  score: 0,
+  cardsMatrix: [[], []],
+  pairs: TOTAL_PAIRS,
+  triesToSelect: 2,
+};
 
 export const memoryGameSlice = createSlice({
-    name: "memoryGame",
-    initialState,
-    reducers: {
-        incrementScore: (state) => {
-            state.score += 1;
-        },
-        incrementFailsCount: (state) => {
-            state.failsCount += 1;
-        },
-        decrementTriesToSelect: (state) => {
-            state.triesToSelect -= 1;
-        },
-        decrementPairs: (state) => {
-            state.pairs -= 1;
-        }
-    }
-})
+  name: "memoryGame",
+  initialState,
+  reducers: {
+    incrementScore: (state) => {
+      state.score += 1;
+    },
+    incrementFailsCount: (state) => {
+      state.failsCount += 1;
+    },
+    decrementTriesToSelect: (state) => {
+      state.triesToSelect -= 1;
+    },
+    decrementPairs: (state) => {
+      state.pairs -= 1;
+    },
+  },
+});
 
-export const {incrementScore, incrementFailsCount, decrementTriesToSelect, decrementPairs} = memoryGameSlice.actions;
+export const {
+  incrementScore,
+  incrementFailsCount,
+  decrementTriesToSelect,
+  decrementPairs,
+} = memoryGameSlice.actions;
 
 export const selectScore = (state: RootState) => state.game.score;
 export const selectFailsCount = (state: RootState) => state.game.failsCount;
