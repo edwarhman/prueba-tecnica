a40a6a2feeeeb58a2815eb761cfab9c20bb10806
Emerson Warhman <edwarhman@gmail.com>
2022-07-10 18:57:55 -0400

    add: layout cards container

--- src/features/card/Card.tsx
+++ src/features/card/Card.tsx
@@ -1,3 +1,11 @@
+import Col from "react-bootstrap/Col";
+import "./Card.css";
+import image from "../../images/question-icon.jpg";
 export function Card(params: { type: string }) {
-  return <div>{params.type}</div>;
+  return (
+    <Col className="card">
+      {params.type}
+      <img src={image} alt="quest" />
+    </Col>
+  );
 }
--- src/features/memoryGame/MemoryGame.tsx
+++ src/features/memoryGame/MemoryGame.tsx
@@ -11,18 +11,33 @@ import {
 } from "./memoryGameSlice";
 import { Card } from "../card/Card";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import "./MemoryGame.css";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export function MemoryGame() {
   const cards = useAppSelector(selectCards);
-  const cardsElements = cards.map((card) => <Card type={card} />);
+  const cardsElements = cards.map((card, idx) => (
+    <Card type={card} key={idx} />
+  ));
   const score = useAppSelector(selectScore);
   const fails = useAppSelector(selectFailsCount);
 
   return (
-    <div>
-      <h2> Score: {score}</h2>
-      <h2>Fails: {fails}</h2>
-      <div className="cardsContainer">{cardsElements}</div>
+    <div className="game">
+      <Row className="statusContainer">
+        <Col>
+          <h2 className="gameScore"> Score: {score}</h2>
+        </Col>
+        <Col>
+          <h2 className="gameFailsCount">Fails: {fails}</h2>
+        </Col>
+      </Row>
+      <Button variant="danger">Click</Button>
+      <div className="cardsContainer border rounded">
+        <Row xs={4}>{cardsElements}</Row>
+      </div>
     </div>
   );
 }
