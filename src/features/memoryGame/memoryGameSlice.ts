6aee5743668559966ace451bb3df1b656dbe8e1e
Emerson Warhman <edwarhman@gmail.com>
2022-07-10 12:27:26 -0400

    feat: create game slice

---
+++ src/features/memoryGame/memoryGameSlice.ts
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { stat } from 'fs';
+import { RootState, AppThunk } from '../../app/store';
+
+const TOTAL_PAIRS = 12;
+
+export interface MemoryGame {
+    failsCount: number,
+    score: number,
+    cardsMatrix: string[][],
+    pairs: number,
+    triesToSelect: number
+}
+
+const initialState: MemoryGame = {
+    failsCount: 0,
+    score: 0,
+    cardsMatrix: [[],[]],
+    pairs: TOTAL_PAIRS,
+    triesToSelect: 2
+}
+
+export const memoryGameSlice = createSlice({
+    name: "memoryGame",
+    initialState,
+    reducers: {
+        incrementScore: (state) => {
+            state.score += 1;
+        },
+        incrementFailsCount: (state) => {
+            state.failsCount += 1;
+        },
+        decrementTriesToSelect: (state) => {
+            state.triesToSelect -= 1;
+        },
+        decrementPairs: (state) => {
+            state.pairs -= 1;
+        }
+    }
+})
+
+export const {incrementScore, incrementFailsCount, decrementTriesToSelect, decrementPairs} = memoryGameSlice.actions;
+
+export const selectScore = (state: RootState) => state.game.score;
+export const selectFailsCount = (state: RootState) => state.game.failsCount;
+export const selectPairs = (state: RootState) => state.game.pairs;
+export const selectTries = (state: RootState) => state.game.triesToSelect;
+
+export default memoryGameSlice.reducer;
