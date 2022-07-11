a1dae82dda6688887886706aa3120ffd9e786383
Emerson Warhman <edwarhman@gmail.com>
2022-07-10 20:12:39 -0400

    feat: change game layout

--- src/features/card/Card.tsx
+++ src/features/card/Card.tsx
@@ -4,7 +4,6 @@ import image from "../../images/question-icon.jpg";
 export function Card(params: { type: string }) {
   return (
     <Col className="card">
-      {params.type}
       <img src={image} alt="quest" />
     </Col>
   );
